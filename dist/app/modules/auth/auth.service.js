50fe7d5b527ee73aa75104b232dcfad0d1ce20fb
farukulWD <farukgb1999@gmail.com>
2025-10-08 00:54:16 +0600

    feat: Implement authentication and role management modules
    - Added auth utility for token creation.
    - Created validation schemas for authentication processes.
    - Developed roles controller with CRUD operations.
    - Defined roles interface and model for MongoDB.
    - Established routes for role management with permission checks.
    - Implemented roles service for database interactions.
    - Added validation schemas for role creation and updates.
    - Developed user controller for admin management.
    - Created user model with necessary fields and methods.
    - Established routes for user management with permission checks.
    - Implemented user service for database interactions.
    - Added validation schemas for user creation.
    - Created utility functions for error handling and response formatting.
    - Integrated Swagger for API documentation.

---
+++ dist/app/modules/auth/auth.service.js
@@ -0,0 +1,385 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthServices = void 0;
+const http_status_1 = __importDefault(require("http-status"));
+const config_1 = __importDefault(require("../../config"));
+const AppError_1 = __importDefault(require("../../errors/AppError"));
+const user_model_1 = require("../user/user.model");
+const auth_utils_1 = require("./auth.utils");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const jsonwebtoken_1 = __importStar(require("jsonwebtoken"));
+const verifyToken_1 = __importDefault(require("../../utils/verifyToken"));
+// import { EmailJobName, emailQueue } from "../../queues/email.queue";
+const generateOTP = () => {
+    // Declare a digits variable
+    // which stores all digits
+    var digits = "0123456789";
+    let OTP = "";
+    for (let i = 0; i < 6; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+};
+const adminLoginFromDB = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const userExist = yield user_model_1.User.isUserExistsByEmail(payload.email);
+    if (!userExist) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "User does not exist");
+    }
+    if (userExist.userType !== "admin") {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "You are not an admin");
+    }
+    if (userExist.isDeleted) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Sorry, your account was deleted");
+    }
+    const matchPassword = yield user_model_1.User.matchUserPassword(payload.password, userExist.password);
+    if (!matchPassword) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Wrong password");
+    }
+    const jwtPayload = {
+        // userRole: userExist.role,
+        email: userExist.email,
+    };
+    const accessToken = (0, auth_utils_1.createToken)({
+        payload: jwtPayload,
+        secret: config_1.default.access_secret,
+        expiresIn: config_1.default.access_token_expires_in,
+    });
+    const refreshToken = (0, auth_utils_1.createToken)({
+        payload: jwtPayload,
+        secret: config_1.default.refresh_secret,
+        expiresIn: config_1.default.refresh_token_expires_in,
+    });
+    return {
+        accessToken,
+        refreshToken,
+        isVerified: userExist.isVerified,
+    };
+});
+const userLoginFromDB = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const userExist = yield user_model_1.User.isUserExistsByEmail(payload.email);
+    if (!userExist) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Wrong email address");
+    }
+    if (userExist.role !== "customer") {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "You are not a customer");
+    }
+    if (userExist.isDeleted) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Sorry, your account was deleted");
+    }
+    const matchPassword = yield user_model_1.User.matchUserPassword(payload.password, userExist.password);
+    if (!matchPassword) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Wrong password");
+    }
+    const jwtPayload = {
+        email: userExist.email,
+    };
+    const accessToken = (0, auth_utils_1.createToken)({
+        payload: jwtPayload,
+        secret: config_1.default.access_secret,
+        expiresIn: config_1.default.access_token_expires_in,
+    });
+    const refreshToken = (0, auth_utils_1.createToken)({
+        payload: jwtPayload,
+        secret: config_1.default.refresh_secret,
+        expiresIn: config_1.default.refresh_token_expires_in,
+    });
+    return {
+        accessToken,
+        refreshToken,
+        isVerified: userExist.isVerified,
+    };
+});
+const refreshToken = (token) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!token) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "You are not authorized", "unauthorized access request");
+    }
+    const decoded = (0, verifyToken_1.default)(token, config_1.default.refresh_secret);
+    if (!decoded) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "You are not authorized", "unauthorized access request");
+    }
+    const user = yield user_model_1.User.isUserExistsByEmail(decoded.email);
+    if (user.isDeleted) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "Your account was deleted", "unauthorized access request");
+    }
+    if (!user.isActive) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "Your account is blocked", "unauthorized access request");
+    }
+    const payload = {
+        userRole: user.role,
+        email: user.email,
+    };
+    const accessToken = (0, auth_utils_1.createToken)({
+        payload,
+        secret: config_1.default.access_secret,
+        expiresIn: config_1.default.access_token_expires_in,
+    });
+    return {
+        accessToken,
+    };
+});
+const forgotPasswordService = (email) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_model_1.User.isUserExistsByEmail(email);
+    if (!user) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "User does not exist");
+    }
+    if (!user.isVerified) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "You are not verified. Please verify your email");
+    }
+    const otpCode = generateOTP();
+    const otpToken = (0, auth_utils_1.createToken)({
+        payload: {
+            otpCode,
+            email: user.email,
+        },
+        secret: config_1.default.otp_secret,
+        expiresIn: config_1.default.otp_expires_in,
+    });
+    yield user_model_1.User.findByIdAndUpdate(user._id, { otp: otpToken });
+    // await emailQueue.add(EmailJobName.sendResetPasswordEmail, {
+    //   user,
+    //   opt: otpCode,
+    // });
+});
+const resetPasswordService = (email, password, otp) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!otp) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "Please enter opt code");
+    }
+    const user = yield user_model_1.User.isUserExistsByEmail(email);
+    if (!user) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "User does not exist");
+    }
+    if (!user.isVerified) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "You are not verified. Please verify your email first.");
+    }
+    let decoded;
+    try {
+        decoded = jsonwebtoken_1.default.verify(user.otp, config_1.default.otp_secret);
+    }
+    catch (err) {
+        if (err instanceof jsonwebtoken_1.TokenExpiredError) {
+            throw new AppError_1.default(http_status_1.default.FORBIDDEN, "OTP has expired");
+        }
+        console.log(err);
+        throw new AppError_1.default(http_status_1.default.FORBIDDEN, "Failed to verify");
+    }
+    if (decoded.otpCode !== otp) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "OTP did not match");
+    }
+    if (email !== decoded.email) {
+        throw new AppError_1.default(http_status_1.default.FORBIDDEN, "Wrong email");
+    }
+    const hashPassword = yield bcrypt_1.default.hash(password, Number(config_1.default.bcrypt_hash_rounds));
+    const updatePasswordRes = yield user_model_1.User.findOneAndUpdate({ email }, { password: hashPassword }).select("-password");
+    return updatePasswordRes;
+});
+const SendVerificationEmailService = (email) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_model_1.User.isUserExistsByEmail(email);
+    if (!user) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "User does not exist");
+    }
+    if (user.isVerified) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "User already verified. Please try signing in.");
+    }
+    const otpCode = generateOTP();
+    const otpToken = (0, auth_utils_1.createToken)({
+        payload: {
+            otpCode,
+            email: user.email,
+        },
+        secret: config_1.default.otp_secret,
+        expiresIn: config_1.default.otp_expires_in,
+    });
+    yield user_model_1.User.findByIdAndUpdate(user._id, { otp: otpToken });
+    // await emailQueue.add(EmailJobName.sendVerificationEmail, {
+    //   user,
+    //   opt: otpCode,
+    // });
+});
+const verifyEmailService = (email, otp) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!otp) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Verification OTP is required", "unauthorized access request");
+    }
+    const user = yield user_model_1.User.isUserExistsByEmail(email);
+    if (!user) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "User does not exist");
+    }
+    if (user.isVerified) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "You are already verified. Please try signing in.");
+    }
+    let decoded;
+    try {
+        decoded = jsonwebtoken_1.default.verify(user.otp, config_1.default.otp_secret);
+    }
+    catch (err) {
+        if (err instanceof jsonwebtoken_1.TokenExpiredError) {
+            throw new AppError_1.default(http_status_1.default.FORBIDDEN, "OTP has expired");
+        }
+        console.log(err);
+        throw new AppError_1.default(http_status_1.default.FORBIDDEN, "Failed to verify");
+    }
+    // const decoded = varifyToken(user.otp, config.otp_secret as string);
+    if (decoded.otpCode !== otp) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "OTP did not match");
+    }
+    if (email !== decoded.email) {
+        throw new AppError_1.default(http_status_1.default.FORBIDDEN, "Wrong email");
+    }
+    const jwtPayload = {
+        // userRole: userExist.role,
+        email: user.email,
+    };
+    const accessToken = (0, auth_utils_1.createToken)({
+        payload: jwtPayload,
+        secret: config_1.default.access_secret,
+        expiresIn: config_1.default.access_token_expires_in,
+    });
+    const refreshToken = (0, auth_utils_1.createToken)({
+        payload: jwtPayload,
+        secret: config_1.default.refresh_secret,
+        expiresIn: config_1.default.refresh_token_expires_in,
+    });
+    const verifiedUser = yield user_model_1.User.findOneAndUpdate({ email }, { isVerified: true, otp: "" }).select("-password");
+    return {
+        accessToken,
+        refreshToken,
+        isVerified: user.isVerified,
+        verifiedUser,
+    };
+});
+const updatePasswordService = (newPassword, currentPassword, email) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_model_1.User.isUserExistsByEmail(email);
+    if (!user) {
+        throw new AppError_1.default(http_status_1.default.UNAUTHORIZED, "User does not exist");
+    }
+    const matchPassword = yield user_model_1.User.matchUserPassword(currentPassword, user.password);
+    if (!matchPassword) {
+        throw new AppError_1.default(http_status_1.default.CONFLICT, "Password does not match");
+    }
+    const password = yield bcrypt_1.default.hash(newPassword, Number(config_1.default.bcrypt_hash_rounds));
+    const updated = yield user_model_1.User.updateOne({ email: user.email }, { password }).select("-password");
+    return updated;
+});
+const getMyDataFromDB = (email, query) => __awaiter(void 0, void 0, void 0, function* () {
+    let select = "";
+    let populate = [];
+    if (query.select) {
+        select = query.select;
+    }
+    else {
+        populate = [
+            {
+                path: "role",
+            },
+        ];
+    }
+    const result = yield user_model_1.User.findOne({ email })
+        .populate(populate)
+        .select(select);
+    return result;
+});
+const createCustomerIntoDB = (customer) => __awaiter(void 0, void 0, void 0, function* () {
+    delete customer.isDeleted;
+    const user = {
+        email: customer.email,
+        password: customer.password,
+        userType: "customer",
+        phoneNumber: customer.phoneNumber,
+        name: customer.name,
+    };
+    const session = yield mongoose_1.default.startSession();
+    try {
+        session.startTransaction();
+        const customerExist = yield user_model_1.User.isUserExistsByEmail(customer.email);
+        if (customerExist) {
+            throw new AppError_1.default(http_status_1.default.CONFLICT, "You are already registered. Try singing in");
+        }
+        const otpCode = generateOTP();
+        const otpToken = (0, auth_utils_1.createToken)({
+            payload: {
+                otpCode,
+                email: user.email,
+            },
+            secret: config_1.default.otp_secret,
+            expiresIn: config_1.default.otp_expires_in,
+        });
+        user.otp = otpToken;
+        const userRes = yield user_model_1.User.create(user);
+        if (!userRes) {
+            throw new AppError_1.default(http_status_1.default.CONFLICT, "failed to register account");
+        }
+        // await emailQueue.add(EmailJobName.sendVerificationEmail, {
+        //   opt: otpCode,
+        //   user,
+        // });
+        yield session.commitTransaction();
+        yield session.endSession();
+        return {
+            user: userRes,
+            verficationSent: true,
+        };
+    }
+    catch (err) {
+        console.log(err);
+        yield session.abortTransaction();
+        yield session.endSession();
+        throw new AppError_1.default(http_status_1.default.CONFLICT, err instanceof AppError_1.default ? err === null || err === void 0 ? void 0 : err.message : "Failed to create user");
+    }
+});
+exports.AuthServices = {
+    adminLoginFromDB,
+    refreshToken,
+    getMyDataFromDB,
+    forgotPasswordService,
+    resetPasswordService,
+    SendVerificationEmailService,
+    verifyEmailService,
+    updatePasswordService,
+    userLoginFromDB,
+    createCustomerIntoDB,
+};
---
+++ dist/app/modules/roles/roles.interface.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EAppFeatures = void 0;
+var EAppFeatures;
+(function (EAppFeatures) {
+    EAppFeatures["role"] = "role";
+    EAppFeatures["user"] = "user";
+})(EAppFeatures || (exports.EAppFeatures = EAppFeatures = {}));
