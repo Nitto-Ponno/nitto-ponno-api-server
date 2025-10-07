b4863364312d09e54a095ceab7389ea5b088c3d1
farukulWD <farukgb1999@gmail.com>
2025-10-08 00:54:31 +0600

    refactor: remove roles and user modules along with their associated files and functionalities

--- dist/app/modules/roles/roles.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EAppFeatures = void 0;
-var EAppFeatures;
-(function (EAppFeatures) {
-    EAppFeatures["role"] = "role";
-    EAppFeatures["user"] = "user";
-})(EAppFeatures || (exports.EAppFeatures = EAppFeatures = {}));
